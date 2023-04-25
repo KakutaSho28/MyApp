87a6547bc38d7c6e76617822718c4772cf6eadf2
KakutaSho28 <kakustagram@icloud.com>
2023-04-25 23:40:16 +0900

    “20230425-自作課題修正-完了”

--- app/resources/views/post_edit.blade.php
+++ app/resources/views/post_edit.blade.php
@@ -21,11 +21,11 @@
                         <form action="{{ route('create.update',['create' => $post['id']])}}" method="post">
                         @csrf
                         @method('PUT')
-                            <label for='title'>タイトル</label>
+                            <label for='title'><span style="font-size: small; color: red;">※</span>タイトル</label>
                                 <input type='text' class='form-control' name='title' value="{{ old('title',$post['title']) }}"/>
-                            <label for='date' class='mt-2'>日付</label>
+                            <label for='date' class='mt-2'><span style="font-size: small; color: red;">※</span>日付</label>
                                 <input type='date' class='form-control' name='date' id='date' value="{{ old('date',$post['date']) }}"/>
-                                <label for="category" class='mt-2'>世代別カテゴリ</label>
+                                <label for="category" class='mt-2'><span style="font-size: small; color: red;">※</span>世代別カテゴリ</label>
                                     <select name="category" id="category" name="category" value="{{ old('category',$post['category']) }}">
                                         <option hidden>選択してください。</option>
                                         <option value="1" @if(1 == $post['category']) selected @endif>U-12</option>
@@ -34,13 +34,13 @@
                                         <option value="0" @if(0 == $post['category']) selected @endif>一般</option>
                                     </select>
                                     <br/>
-                                <label for='text' class='mt-2'>ワークアウト内容</label>
+                                <label for='text' class='mt-2'><span style="font-size: small; color: red;">※</span>ワークアウト内容</label>
                                 <input type='text' class='form-control' name='workout' id='workout' value="{{ old('workout',$post['workout']) }}"/>
-                                <label for='spot' class='mt-2'>開催場所</label>
+                                <label for='spot' class='mt-2'><span style="font-size: small; color: red;">※</span>開催場所</label>
                                 <input type='text' class='form-control' name='spot' id='spot' value="{{ old('spot',$post['spot']) }}"/>
-                                <label for='price' class='mt-2'>料金</label>
+                                <label for='price' class='mt-2'><span style="font-size: small; color: red;">※</span>料金</label>
                                 <input type='text' class='form-control' name='price' id='price' value="{{ old('price',$post['price']) }}"/>
-                                <label for='text' class='mt-2'>備考</label>
+                                <label for='text' class='mt-2'><span style="font-size: small; color: red;">※</span>備考</label>
                                 <textarea class='form-control' name='text' id='text' value="">{{ old('text',$post->text) }}</textarea>
                                 <div class='row justify-content-center'>
                                     <button type='submit' class='btn  w-25 mt-3' onclick="return confirm('編集しますか？');">編集</button>
